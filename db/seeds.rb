058f5619c3d77bcf48aa61395492a6646a0b796f
Marcel Fonseca <marcelsf23br@gmail.com>
2018-11-13 20:42:52 +0100

    Everything works

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+10.times do
+article = Article.new()
+article.title = Faker::Coffee.blend_name
+article.content = Faker::Coffee.origin
+article.save
+end
+
